f1efff265a685fe92bc3678730e374dea1c3460b
Pi-Tan Hu <4020428+LaContra@users.noreply.github.com>
2021-05-06 23:13:58 +0000

    Add content-toggle and toggled-panel shortcode.

--- static/js/tab.js
+++ static/js/tab.js
@@ -1,16 +1,24 @@
 $(document).ready(function () {
-    $('.nav-tabs li button').click(function (e) {
-        e.preventDefault();
-
-        // Remove active class for all tabs and tab panels.
-        $('.nav-tabs li').find('.active').removeClass('active').attr("aria-selected", "false");
-        $('.tab-content').find('.active').removeClass('active');
-
-        var tabName = $(this).attr("title").replace("tab-", "");
-
-        // Add active class for all tabs and tab panels with the same name.
-        $(".nav-tabs button[title='tab-" + tabName + "']")
-            .addClass('active').attr("aria-selected", "true");
-        $(".tab-content div[title='tab-pane-" + tabName + "']").addClass('active');
+    $(".nav-tabs li button").click(function (event) {
+        updateStyle(event, this, ".nav-tabs", ".tab-content", "tab");
     });
+
+    $(".content-toggle li button").click(function (event) {
+        updateStyle(event, this, ".content-toggle", ".toggle-panel", "toggle");
+    });
+
+    function updateStyle(event, target, itemsClass, panelsClass, itemPrefix) {
+        event.preventDefault();
+
+        // Remove active class for all items and panels.
+        $(itemsClass + " li").find(".active").removeClass("active").attr("aria-selected", "false");
+        $(panelsClass).find(".active").removeClass("active");
+
+        var itemName = $(target).attr("title").replace(itemPrefix, "");
+
+        // Add active class for all items and panels with the same name.
+        $(itemsClass + " button[title='" + itemPrefix + itemName + "']")
+            .addClass("active").attr("aria-selected", "true");
+        $(panelsClass + " div[title='" + itemPrefix + "-pane" + itemName + "']").addClass("active");
+    }
 });
